d327e389e63b36ec8bd7e9ee4901bfee1f859792
seungwonme <seungwonan.me@gmail.com>
2024-04-15 08:11:57 +0900

    Implement MyVector class

--- seungwon/1_array/MyVector/src/main.cpp
+++ seungwon/1_array/MyVector/src/main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
-#include "MyStack.tpp"
+#include "MyVector.tpp"
+using namespace std;
 
-int main(void)
+MyVector<int> v;
+
+void capacity_test(void)
 {
-    MyStack<int> st;
-    st.push(3);
-    std::cout << st.top() << st.empty() << st.size() << '\n';
-    st.pop();
-    std::cout << st.empty() << st.size() << '\n';
+    MyVector<int> nv(10);
+    cout  << "nv.capacity() : " << nv.capacity() << '\n'
+    << "nv.size() : " << nv.size() << '\n';
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << nv[i] << ' ';
+    }
+
+    cout << "\n----------------CAPACITY----------------\n";
+    for (size_t i = 1; i <= 100; i++)
+    {
+        v.push_back(i);
+        cout << v.capacity() << '\n';
+    }
+    cout << "----------------------------------------\n";
+
+    v.resize(10);
+    v.push_back(1);
+    v.push_back(2);
+    cout << "v.size() : " << v.size() << '\n';
+    cout << "v.capacity() : " << v.capacity() << '\n';
+    for (int i = 0; i < 100; i++)
+    {
+        cout << v[i] << ' ';
+    }
+    v.at(0);
+}
+
+int	main(void)
+{
+    capacity_test();
+    return 0;
 }
