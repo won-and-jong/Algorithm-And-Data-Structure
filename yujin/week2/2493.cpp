4b92599d79c738030af8cad3fb4d0963a7318a29
성유진 <syj010820@naver.com>
2024-04-15 15:22:31 +0900

    [2493 탑] 2try

--- yujin/week2/2493.cpp
+++ yujin/week2/2493.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-stack<int> T;
-stack<int> tmp;
+stack<int> S;
+int T[500001];
 int result[500001];
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		int h;
 		cin >> h;
-		T.push(h);
+		T[i] = h;
 	}
-	for (int i = n; i > 0; i--) {
-		int now = T.top(); T.pop();
-		while (!T.empty() && now > T.top()) {
-			tmp.push(T.top()); T.pop();
-		}
-		result[i] = i - (tmp.size() + 1);
-		while (!tmp.empty()) {
-			T.push(tmp.top());
-			tmp.pop();
-		}
+	for (int i = 1; i <= n; i++) {
+		//T[i]:확인하려는 탑의 높이, T[S.top()]:스택의 맨위에 존재하는 탑의 높이
+		while (!S.empty() && T[i] > T[S.top()])
+			S.pop();
+		if (!S.empty() && T[i] < T[S.top()])
+			result[i] = S.top();
+		S.push(i); // 탑의 번호를 스택에 저장
 	}
 	for (int i = 1; i <= n; i++)
 		cout << result[i] << " ";
