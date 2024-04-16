c6ee07c57242f0b2e3373df2301b1aa500275ab0
성유진 <syj010820@naver.com>
2024-04-16 18:35:29 +0900

    제로 코드 수정

--- yujin/stack(week2)/10773_제로/10773.cpp
+++ yujin/stack(week2)/10773_제로/10773.cpp
@@ -9,38 +9,20 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N;
-	cin >> N;
-	while (N--) {
-		string cmd;
-		cin >> cmd;
-		if (cmd == "push") {
-			int x;
-			cin >> x;
-			S.push(x);
-		}
-		else if (cmd == "pop") {
-			if (S.empty()) {
-				cout << -1 << "\n";
-				continue;
-			}
-			cout << S.top() << "\n";
+	int K;
+	cin >> K;
+	while (K--) {
+		int x;
+		cin >> x;
+		if (x == 0)
 			S.pop();
-		}
-		else if (cmd == "size")
-			cout << S.size() << "\n";
-		else if (cmd == "empty") {
-			if (S.empty())
-				cout << 1 << "\n";
-			else
-				cout << 0 << "\n";
-		}
-		else if (cmd == "top") {
-			if (S.empty()) {
-				cout << -1 << "\n";
-				continue;
-			}
-			cout << S.top() << "\n";
-		}
+		else
+			S.push(x);
 	}
+	int result = 0;
+	while (!S.empty()) {
+		result += S.top();
+		S.pop();
+	}
+	cout << result;
 }
